35a364801dc07421c582316a496f0e2f464a4711
Armaan Gupta <151835988+armaanngupta@users.noreply.github.com>
2025-06-04 16:40:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks.js')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middlewares
+
+app.use(express.json())
+app.use(express.static('./public'))
+//routes
+
+
+
+app.use('/api/v1/tasks', tasks)
+const port = 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening at ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
