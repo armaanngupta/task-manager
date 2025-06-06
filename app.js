ab6eac3e581eb33fb8e3c6affa1e903f18d46358
Armaan Gupta <151835988+armaanngupta@users.noreply.github.com>
2025-06-06 14:24:21 +0530

    rearranged

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ require('dotenv').config()
 //middlewares
 app.use(express.json())
 app.use(express.static('./public'))
-// app.use((req,res,next)=> {
-//     console.log(req.url)
-//     // next()
-// })
+
 //routes
 app.use('/api/v1/tasks', tasks)
 app.use(notFound)
